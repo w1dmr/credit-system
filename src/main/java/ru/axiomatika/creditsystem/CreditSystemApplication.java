ba732f786bef3c5c9978c0d226f5feddaf28ec12
w1dmr <v.v.volkov.03@gmail.com>
2025-03-14 21:49:51 +0300

    init commit

---
+++ src/main/java/ru/axiomatika/creditsystem/CreditSystemApplication.java
@@ -0,0 +1,12 @@
+package ru.axiomatika.creditsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditSystemApplication.class, args);
+    }
+}
---
+++ src/test/java/ru/axiomatika/creditsystem/CreditSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.axiomatika.creditsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
